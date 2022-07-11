eb6443709f097bae6c6925bf10b918cde4b496cf
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-07-11 17:38:55 -0500

    about description added

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,19 +4,29 @@ export interface IAboutProps {}
 
 export default function About(props: IAboutProps) {
   return (
-    <section>
-      <h3 id="about-me" className="title">
+    <section className="block columns is-centered is-multiline is-vcentered">
+      <h3 id="about-me" className="title column is-full has-text-white m-2">
         What am I about...
       </h3>
-      <figure className="image">
+      <figure className="image column is-one-quarter is-6-mobile">
         <img
           src={headshot}
           alt="headshot"
-          style={{ width: "30%" }}
+          style={{ width: "100%" }}
           className="is-rounded"
         />
       </figure>
-      <h3 className="">Test</h3>
+      <p className="column is-size-5 has-text-white m-2">
+        I'm a new junior developer fresh out of coding bootcamp. I'm leaving a
+        five year career in Phlebotomy and blood product donation to persue a
+        career in web development and I'm ready to take on new opportunities and
+        challenges.
+        <br /> <br />I am experienced in using HTML, CSS, Javascript, and
+        Typescript to create mobile first progressive web apps within a MVC
+        standard and within React. I'm ready and excited to continue expanding
+        on my development prowess in both the aformentioned frameworks and
+        beyond. Check out some of my work!
+      </p>
     </section>
   );
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -3,13 +3,36 @@ import * as React from "react";
 export interface INavProps {}
 
 export default function Nav(props: INavProps) {
+  const navSelect = function (event: React.MouseEvent) {
+    console.log(event.currentTarget.textContent);
+  };
+
   return (
-    <header className="block columns bg-secondary-light is-centered has-text-centered">
-      <a className="column has-text-white has-text-weight-bold">About Me</a>
-      <a className="column has-text-white has-text-weight-bold">
+    <header className="block columns bg-secondary-light is-centered is-vcentered has-text-centered">
+      <h2 className="column title primary-alt">
+        The Works of <br /> Carl McGee
+      </h2>
+      <a
+        className="column has-text-white has-text-weight-bold"
+        id="about"
+        onClick={(e) => navSelect(e)}
+      >
+        About Me
+      </a>
+      <a
+        className="column has-text-white has-text-weight-bold"
+        id="work"
+        onClick={(e) => navSelect(e)}
+      >
         Previous Work
       </a>
-      <a className="column has-text-white has-text-weight-bold">Contact</a>
+      <a
+        className="column has-text-white has-text-weight-bold"
+        id="contact"
+        onClick={(e) => navSelect(e)}
+      >
+        Contact
+      </a>
     </header>
   );
 }
