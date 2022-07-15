f9207e847c437b1872e4d1ca3267b8c92cddd0ae
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-07-15 10:52:40 -0500

    added github logo with link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
+import githubLogo from "./assets/PNG/GitHub-Mark-64px.png";
 
 export interface IFooterProps {}
 
 export default function Footer(props: IFooterProps) {
   return (
-    <footer className="footer colunms bg-primary-dark">
-      <h3 className="column is-size-7 has-text-centered">© 2022 Carl McGee</h3>
+    <footer className="footer columns is-centered bg-primary-dark">
+      <div className="column is-2 columns">
+        <a href="https://github.com/CarlJMcGee">
+          <img src={githubLogo} alt="" />
+        </a>
+      </div>
+      <h3 className="column is-2 has-text-centered">© 2022 Carl McGee</h3>
     </footer>
   );
 }
