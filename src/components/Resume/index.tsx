0b4fcc954e56a7e81b65dcc19943b9452e8db20b
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-07-17 10:48:43 -0500

    styled and fomatted resume page

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -5,15 +5,26 @@ const resume = require("./assets/Resume-2022.pdf");
 
 export default function Resume(props: IResumeProps) {
   return (
-    <div className="block column columns is-centered is-multiline">
+    <div className="block column columns is-centered is-multiline is-mobile">
       <a
         href={resume}
         download={"CarlJMcGee-Resume"}
-        className="column columns is-centered"
+        className="column columns is-centered is-mobile"
       >
-        <button className="button column is-one-third p-1">Resume</button>
+        <button
+          className={`button column ${
+            window.innerWidth >= 550 ? "is-one-third" : ""
+          } bg-secondary-dark p-1`}
+        >
+          Download My Resume
+        </button>
       </a>
-      <ol className="column is-full is-size-5" style={{ listStyle: "inside" }}>
+      <ol
+        className={`column is-full is-size-5 has-text-white ${
+          window.innerWidth >= 550 ? "px-6" : ""
+        }`}
+        style={{ listStyle: "inside" }}
+      >
         <li>Experienced in organizing and leading groups of people</li>
         <li>
           Can break down complex ideas and explain them in easy to understand
@@ -22,7 +33,7 @@ export default function Resume(props: IResumeProps) {
         <li>Quick to pick up on new ideas and information </li>
         <li>
           Proficient in HTML, CSS, Javascript, JQuery, Express servers via Node
-          JS, MySQL using Sequelize, and MongoDB using Mongoose
+          JS, MySQL using Sequelize, and MongoDB using Mongoose, and React
         </li>
         <li>
           Capable of producing full stack applications using MVP practices
